db9d83f41d6df24142af951f4313e1b6290a2e27
jaejung <santoki389@gmial.com>
2024-02-13 18:08:35 +0900

    gugudan

---
+++ src/main/java/com/ohgiraffers/section02/looping/Application01.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+//        a.testSimpleForStatement();
+//        a.testForExample1();
+//        a.testForExample2();
+//        a.testForExample3();
+//        a.printSimpleGugudan();
+
+       A_nestedFor a2 = new A_nestedFor();
+//        a2.printGugudanFromWeoToNine();
+//        a2.printUpgradeGugudan();
+        a2.printStarInputRowTimes();
+    }
+
+
+
+}
